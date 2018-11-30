f2f86904a045a871ecef8488d1e1fb54f8f62d01
Christopher Czach <chrisczach@gmail.com>
2018-11-29 22:51:44 -0800

    Updates

---
+++ precache-manifest.38c854223426ac40576ea2c2b8484f6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "81902a590213c893c0e6",
+    "url": "/winniepuzzle/static/css/main.6d373d4b.chunk.css"
+  },
+  {
+    "revision": "81902a590213c893c0e6",
+    "url": "/winniepuzzle/static/js/main.81902a59.chunk.js"
+  },
+  {
+    "revision": "4e1b96fda10113cc8603",
+    "url": "/winniepuzzle/static/js/1.4e1b96fd.chunk.js"
+  },
+  {
+    "revision": "ce36a176cd1504c1290b",
+    "url": "/winniepuzzle/static/js/runtime~main.ce36a176.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/winniepuzzle/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c3e5e7e50b4e9d9fc7186e9d9b11d3a0",
+    "url": "/winniepuzzle/index.html"
+  }
+];
