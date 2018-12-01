d8a00323fccb0e2f186c197d76187c88edcf36d7
Christopher Czach <chrisczach@gmail.com>
2018-11-30 21:39:39 -0800

    add splash

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Board from './board/board';
 import Tile from './tile/tile';
+import Splash from './splash/splash';
+import classes from './app.module.css';
 
 //`./img/${props.item.item}`
 
@@ -21,103 +21,113 @@ const images = [
   { id: 12, item: 'winnie12.jpg' }
 ];
 
-const updateAvailableMovesUtility = (state) => {
-    const currIndex = state.board.findIndex(e=> e.imageRow === 'X');
-    const possibleMoves = [];
-    if ((currIndex + 1 )% state.boardSize !== 0) possibleMoves.push(currIndex + 1)
-    if (currIndex % state.boardSize !== 0) possibleMoves.push(currIndex - 1)
-    if (currIndex - state.boardSize >= 0) possibleMoves.push(currIndex - state.boardSize)
-    if (currIndex + state.boardSize < state.board.length) possibleMoves.push(currIndex + state.boardSize)
-    return {...state, possibleMoves}
-}
+const updateAvailableMovesUtility = state => {
+  const currIndex = state.board.findIndex(e => e.imageRow === 'X');
+  const possibleMoves = [];
+  if ((currIndex + 1) % state.boardSize !== 0)
+    possibleMoves.push(currIndex + 1);
+  if (currIndex % state.boardSize !== 0) possibleMoves.push(currIndex - 1);
+  if (currIndex - state.boardSize >= 0)
+    possibleMoves.push(currIndex - state.boardSize);
+  if (currIndex + state.boardSize < state.board.length)
+    possibleMoves.push(currIndex + state.boardSize);
+  return { ...state, possibleMoves };
+};
 
 const moveTileUtility = (state, index) => {
-  
-  const currIndex = state.board.findIndex(e => e.imageRow === 'X')
-  if (state.possibleMoves.indexOf(index - 1) === -1) return
+  const currIndex = state.board.findIndex(e => e.imageRow === 'X');
+  if (state.possibleMoves.indexOf(index - 1) === -1) return;
   const board = state.board;
   [board[index - 1], board[currIndex]] = [board[currIndex], board[index - 1]];
   const moveHistory = [...state.moveHistory, currIndex];
 
-  state = {...state, board, moveHistory}
-
-  state = checkBoardUtility(state);
+  state = { ...state, board, moveHistory };
   state = updateAvailableMovesUtility(state);
 
   return state;
-}
+};
 
-const checkBoardUtility = (state) => {
+const checkBoardUtility = state => {
   let win = true;
+  let currGame = state.currGame;
   state.board.forEach((e, i) => {
-    const homeIndex = ((e.imageRow - 1) * state.boardSize) + (e.imageColumn - 1);
-    if (homeIndex !== i && e.imageRow !== 'X') { win = false }
+    const homeIndex = (e.imageRow - 1) * state.boardSize + (e.imageColumn - 1);
+    if (homeIndex !== i && e.imageRow !== 'X') {
+      win = false;
+    }
   });
-  return {...state, win};
-}
+  if (win) {currGame = false;}
+  return { ...state, win, currGame };
+};
 
-const setBoardUtility = (state) => {
+const setBoardUtility = state => {
   const board = [];
   for (let imageRow = 1; imageRow <= state.boardSize; imageRow++) {
     for (let imageColumn = 1; imageColumn <= state.boardSize; imageColumn++) {
       if (imageColumn === state.boardSize && imageRow === state.boardSize) {
-        board.push({ imageRow: 'X', imageColumn: 'X' })
+        board.push({ imageRow: 'X', imageColumn: 'X' });
       } else {
         board.push({ imageRow, imageColumn });
       }
-      
     }
   }
-  return {...state, board, openSpot: state.boardSize ** 2 - 1 }
+  return {
+    ...state,
+    board,
+    openSpot: state.boardSize ** 2 - 1,
+    moveHistory: []
+  };
+};
 
-}
-
-const newGameUtility = (state,numTiles, numMoves) => {
+const newGameUtility = (state, numTiles, level) => {
   const image = images[Math.floor(Math.random() * images.length)].item;
   const win = false;
-  const moves = numMoves;
   const boardSize = numTiles;
   const currGame = true;
-  return {...state, image, win, moves, boardSize, currGame}
-}
+  return { ...state, image, win, level, boardSize, currGame };
+};
 
-const randomizeBoardUtility = (state, numMoves) => {
-  for (let i = 0; i < numMoves * 10; i++) {
+const randomizeBoardUtility = (state, level) => {
+  for (let i = 0; i < level * 10; i++) {
     const moves = state.possibleMoves;
     const lastMove = state.moveHistory[state.moveHistory.length - 1];
     const tile = () => {
       if (moves[Math.floor(Math.random() * moves.length)] === lastMove) {
-        return tile()
-      } else { return moves[Math.floor(Math.random() * moves.length)] }
-    }
-  
-    state = moveTileUtility(state, tile() + 1);
-    state = checkBoardUtility(state);
-    state = updateAvailableMovesUtility(state);
+        return tile();
+      } else {
+        return moves[Math.floor(Math.random() * moves.length)];
+      }
+    };
 
+    state = moveTileUtility(state, tile() + 1);
   }
 
-  return state
-
-
-}
+  return state;
+};
 
 class App extends Component {
   state = {
-    image: 'winnie1.jpg',
-    tileSize: '',
-    possibleMoves: [],
-    moveHistory: [],
+    level: 1,
     boardSize: 4,
-    board: [],
-    openSpot: 0,
+    currGame: false,
     win: false
-  
   };
 
+  setOptions = (incLevel, incBoardSize) => {
+    this.setState(state => {
+      const level = Math.min(Math.max(state.level + incLevel, 1),10);
+      const boardSize = Math.min(Math.max(state.boardSize + incBoardSize,3),10);
+      return {...state, level, boardSize}
+    })
+  }
+
   setTile = () => {
-    this.setState(state=> {
-      return { tileSize: Math.min(window.innerHeight, window.innerWidth)*.98 / state.boardSize }
+    this.setState(state => {
+      return {
+        tileSize:
+          (Math.min(window.innerHeight, window.innerWidth) * 0.98) /
+          state.boardSize
+      };
     });
   };
 
@@ -125,63 +135,72 @@ class App extends Component {
     this.setState(state => {
       state = setBoardUtility(state);
       state = updateAvailableMovesUtility(state);
-
-
       return state;
-    })
-  }
+    });
+  };
 
   updateAvailableMoves = () => {
-    this.setState(state => updateAvailableMovesUtility(state))
-    
-  }
+    this.setState(state => updateAvailableMovesUtility(state));
+  };
 
   moveTile = (index, e) => {
     this.setState(state => {
       state = moveTileUtility(state, index);
-      return state
+      state = checkBoardUtility(state);
+      return state;
     });
-  }
-
-  checkBoard = () => {
-    this.setState(state=> checkBoardUtility(state));
-  }
-
-  newGame = (numTiles, numMoves) => {
-    this.setState(state => {
-      state = newGameUtility(state, numTiles, numMoves);
-      state = setBoardUtility(state);
-      state = updateAvailableMovesUtility(state);
-      state = randomizeBoardUtility(state, numMoves);
-      return state
-      });
   };
 
+  checkBoard = () => {
+    this.setState(state => checkBoardUtility(state));
+  };
 
-
- 
-
-
-
+  newGame = () => {
+    this.setState(state => {
+      state = newGameUtility(state, state.boardSize, state.level);
+      state = setBoardUtility(state);
+      state = updateAvailableMovesUtility(state);
+      state = randomizeBoardUtility(state, state.level);
+      return state;
+    });
+  };
 
   componentDidMount() {
-    this.newGame(4, 4);
+    //this.newGame(this.state.boardSize, this.state.moves);
     this.setTile();
     window.addEventListener('resize', this.setTile);
   }
 
   componentWillUnmount() {
     window.removeEventListener('resize', this.setTile);
-
   }
 
   render() {
     return (
-      <Board gridSize={this.state.boardSize}>
-        {this.state.board.map((e, i) => <Tile clickHandler={this.moveTile}  {...e} image={this.state.image} boardSize={this.state.boardSize} index={i + 1} size={this.state.tileSize}/>)}
-      </Board>
-
-    )
+      <div className={classes.main}>
+        <Splash
+          setOptions={this.setOptions}
+          level={this.state.level}
+          boardSize={this.state.boardSize}
+          newGame={this.newGame}
+          currGame={this.state.currGame}
+          win={this.state.win}
+        />
+        <Board gridSize={this.state.boardSize}>
+          {this.state.board &&
+            this.state.board.map((e, i) => (
+              <Tile
+                clickHandler={this.moveTile}
+                {...e}
+                image={this.state.image}
+                boardSize={this.state.boardSize}
+                index={i + 1}
+                size={this.state.tileSize}
+              />
+            ))}
+        </Board>
+      </div>
+    );
   }
 }
 
---
+++ src/splash/splash.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import classes from './splash.module.css';
+
+const Splash = props => {
+  const levelUp = () => props.setOptions(1, 0);
+  const levelDown = () => props.setOptions(-1, 0);
+  const sizeUp = () => props.setOptions(0, 1);
+  const sizeDown = () => props.setOptions(0, -1);
+
+
+
+    return (
+      <div className={!props.currGame ? `${classes.shown} ${classes.splash}` : classes.splash}>
+        <div className={classes.center}>
+          <h1>Winnie Shuffle!!</h1>
+          {props.win && <h2>You Win!</h2>}
+
+          <h3>Select Level</h3>
+          <div>
+            <div className={classes.incButton} onClick={levelUp}>&uarr;</div>
+            <div className={classes.display}>{props.level}</div>
+            <div className={classes.incButton} onClick={levelDown}>&darr;</div>
+          </div>
+
+          <h3>Select Grid Size</h3>
+          <div>
+            <div className={classes.incButton} onClick={sizeUp}>&uarr;</div>
+            <div className={classes.display}>{props.boardSize}</div>
+            <div className={classes.incButton} onClick={sizeDown}>&darr;</div>
+          </div>
+          <div onClick={props.newGame} className={classes.start}>Start a New Game &rarr;</div>
+        </div>
+      
+      </div>
+    )
+
+
+
+}
+
+export default Splash;
