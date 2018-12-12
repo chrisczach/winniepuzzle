506e14480382a1ebbbb2c971ea856be509eb0f1e
Chris Czach <chrisczach@gmail.com>
2018-12-12 15:09:24 -0800

    Updates

--- precache-manifest.2720f9fe666184857c8d80ff93d3352e.js
+++ precache-manifest.2720f9fe666184857c8d80ff93d3352e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/winniepuzzle/static/js/runtime~main.ce36a176.js"
   },
   {
-    "revision": "be58a42667748d5c3587",
-    "url": "/winniepuzzle/static/js/main.be58a426.chunk.js"
+    "revision": "0a4d6a75e0b6d79af294",
+    "url": "/winniepuzzle/static/js/main.0a4d6a75.chunk.js"
   },
   {
     "revision": "4182652c5cf8d6503a34",
     "url": "/winniepuzzle/static/js/1.4182652c.chunk.js"
   },
   {
-    "revision": "be58a42667748d5c3587",
-    "url": "/winniepuzzle/static/css/main.ade4cc0e.chunk.css"
+    "revision": "0a4d6a75e0b6d79af294",
+    "url": "/winniepuzzle/static/css/main.8c9f1de8.chunk.css"
   },
   {
-    "revision": "404a2ef823c5d75265fb8e93f324fba0",
+    "revision": "8be49ec5a0990eddfcd27e11681af523",
     "url": "/winniepuzzle/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/winniepuzzle/precache-manifest.3dd6766ea4ccdcdbd38efb6c1cb624d7.js"
+  "/winniepuzzle/precache-manifest.2720f9fe666184857c8d80ff93d3352e.js"
 );
 
 workbox.clientsClaim();
