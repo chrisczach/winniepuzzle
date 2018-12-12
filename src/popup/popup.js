4f66a984a6a3dce18b63c7838559edf145bbd92a
Chris Czach <chrisczach@gmail.com>
2018-12-12 15:18:00 -0800

    format

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -3,7 +3,7 @@ import classes from './popup.module.css';
 
 const Popup = props => {
   const shown = props.currGame && props.showInstall === true ? classes.show : classes.hide;
-  return <div onClick={props.closePopup} className={shown + ' ' + classes.popup}><p> To install this app on your iPhone, tap  <img align="middle"  src="./nav/action.png"  />  and then tap Add to Homescreen.</p>
+  return <div onClick={props.closePopup} className={shown + ' ' + classes.popup}><p> To install this app on your iPhone, tap  <img align="middle"  src="./nav/action.png"  />  and then tap <span>"Add to Home Screen."</span></p>
   <div className={classes.close}> &nbsp;</div>
   </div>;
 };
