169dbdf98da1e9a1cbde258f344c2eede6a08b64
Chris Czach <chrisczach@gmail.com>
2018-12-12 13:32:51 -0800

    trying to add loading screen

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -3,7 +3,9 @@ import classes from './popup.module.css';
 
 const Popup = props => {
   const shown = props.currGame && props.showInstall === true ? classes.show : classes.hide;
-  return <div onClick={props.closePopup} className={shown + ' ' + classes.popup}><p> To install this app on your iPhone, tap <img align="middle"  src="./nav/action.png"  /> and then Add to homescreen.</p></div>;
+  return <div onClick={props.closePopup} className={shown + ' ' + classes.popup}><p> To install this app on your iPhone, tap  <img align="middle"  src="./nav/action.png"  />  and then tap Add to Homescreen.</p>
+  <div className={classes.close}> &nbsp;</div>
+  </div>;
 };
 
 export default Popup;
