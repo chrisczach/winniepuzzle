a973d007b331d01b1be0f810474affc5c9575123
Chris Czach <chrisczach@gmail.com>
2018-12-12 10:14:50 -0800

    Updates

--- precache-manifest.470ddf93abdacb63a33e413a9da86810.js
+++ precache-manifest.470ddf93abdacb63a33e413a9da86810.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/winniepuzzle/static/js/runtime~main.ce36a176.js"
   },
   {
-    "revision": "59167817552ba1f1a5b6",
-    "url": "/winniepuzzle/static/js/main.59167817.chunk.js"
+    "revision": "06a18e1daf08d2cc55be",
+    "url": "/winniepuzzle/static/js/main.06a18e1d.chunk.js"
   },
   {
     "revision": "4182652c5cf8d6503a34",
     "url": "/winniepuzzle/static/js/1.4182652c.chunk.js"
   },
   {
-    "revision": "59167817552ba1f1a5b6",
+    "revision": "06a18e1daf08d2cc55be",
     "url": "/winniepuzzle/static/css/main.e1a4615d.chunk.css"
   },
   {
-    "revision": "83864fe59f5cf3a4f3ec8c665c1442d2",
+    "revision": "e03e90b96728231ad5d2ae30839227ba",
     "url": "/winniepuzzle/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/winniepuzzle/precache-manifest.4c6b98af39deecbfe48440486cbbd1a9.js"
+  "/winniepuzzle/precache-manifest.470ddf93abdacb63a33e413a9da86810.js"
 );
 
 workbox.clientsClaim();
